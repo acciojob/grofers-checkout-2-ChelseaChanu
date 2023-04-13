8d3cfebb5964726e91799728c44fbf08636860a1
Chelsea <88430660+ChelseaChanu@users.noreply.github.com>
2023-04-13 21:50:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+const table = document.getElementById("table");
+const price = document.querySelectorAll(".prices");
+let total = 0;
+for(let amount of price){
+	total += parseFloat(amount.innerText);
+}
+
+const row = table.insertRow(-1);
+const cell1 = row.insertCell(0);
+const cell2 = row.insertCell(1);
+cell1.innerText = "Grand Total";
+cell2.innerText = total;
+cell2.setAttribute('id','grandTotal');
